69330a8d435adccffd81aa93b16c6b991bf09b2e
DESKTOP-N7UMBP9\AbelChan <abelchan@DESKTOP-N7UMBP9>
2024-11-29 08:51:58 +0800

    sproto 代码上传
    添加所有方法， 添加枚举

---
+++ dist/sprotoConst.ts
@@ -0,0 +1,12 @@
+
+export const SPROTO_REQUEST: number = 0;
+export const SPROTO_RESPONSE: number = 1;
+
+// sproto_arg.type
+export enum SprotoTagType {
+    SPROTO_TINTEGER = 0,
+    SPROTO_TBOOLEAN,
+    SPROTO_TSTRING,
+    SPROTO_TDOUBLE,
+    SPROTO_TSTRUCT
+}
