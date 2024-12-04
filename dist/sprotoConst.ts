f281f711e6b037e8880c228f051649b389f506d8
DESKTOP-N7UMBP9\AbelChan <abelchan@DESKTOP-N7UMBP9>
2024-12-04 08:44:13 +0800

    添加 枚举

--- dist/sprotoConst.ts
+++ dist/sprotoConst.ts
@@ -1,6 +1,15 @@
 
 export const SPROTO_REQUEST: number = 0;
 export const SPROTO_RESPONSE: number = 1;
+export const SPROTO_TARRAY = 0x80;
+export const CHUNK_SIZE = 1000;
+export const SIZEOF_LENGTH = 4;
+export const SIZEOF_HEADER = 2;
+export const SIZEOF_FIELD = 2;
+
+export const ENCODE_BUFFERSIZE = 2050;
+export const ENCODE_MAXSIZE = 0x1000000;
+export const ENCODE_DEEPLEVEL = 64;
 
 // sproto_arg.type
 export enum SprotoTagType {
@@ -9,4 +18,15 @@ export enum SprotoTagType {
     SPROTO_TSTRING,
     SPROTO_TDOUBLE,
     SPROTO_TSTRUCT
+}
+
+export enum StringSubType {
+    TSTRING_STRING = 0,
+    TSTRING_BINARY = 1
+}
+
+export enum SprotoCBType {
+    SPROTO_CB_ERROR = -1,
+    SPROTO_CB_NULL = -2,
+    SPROTO_CB_NOARRAY = -3
 }
